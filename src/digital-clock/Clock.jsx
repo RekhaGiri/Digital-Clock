a752bc611025a4442baa214dca3cc479f60f441d
Rekha Giri <rekhagirigiri84@gmail.com>
2023-10-24 12:21:28 +0530

    Digital-clock Added

---
+++ src/digital-clock/Clock.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import './Clock.css';
+
+function Clock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  const formattedTime = time.toLocaleTimeString();
+
+  return (
+    <div class="main">
+    <div className="clock">
+      <h4>Digital Clock</h4>
+      <p>{formattedTime}</p>
+    </div>
+    </div>
+  );
+}
+
+export default Clock;
